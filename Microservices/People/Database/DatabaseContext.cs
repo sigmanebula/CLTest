9efab7e36d76eed34a7148e2a31c41e54a912543
Nikolas <nikolas-9449@yandex.ru>
2021-01-28 00:11:46 +0700

    People Get Ready

---
+++ Microservices/People/Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=.; initial catalog=Microservices; persist security info=True; user id=sa; password=GDc6GlPQTNMSLPOwNQil;");
+        }
+    }
+}
---
+++ Microservices/People/Database/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People.Database
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Thumbnail { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Quote { get; set; }
+        public string Poem { get; set; }
+        public double Distance { get; set; }
+
+    }
+}
+
